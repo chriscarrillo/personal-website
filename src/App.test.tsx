fc381c77b35b09562979cdb9dbbeace553c303fa
Chris Carrillo <chriscarrillo@outlook.com>
2020-12-12 18:51:45 -0600

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import {describe, expect, test} from '@jest/globals'
+import {render, screen} from '@testing-library/react'
+import React from 'react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('<App />', () => {
+  test('renders learn react link', () => {
+    render(<App />)
+    const linkElement = screen.getByText(/learn react/i)
+    expect(linkElement).toBeTruthy()
+  })
+})
